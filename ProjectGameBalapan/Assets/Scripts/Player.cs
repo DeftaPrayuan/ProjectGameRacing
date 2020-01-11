942c9d325ebc29eee68b2ea2061bdcc91b3a78eb
R-Lan Kurnia <33871129+krlan2789@users.noreply.github.com>
2020-01-11 18:19:30 +0700

    Menambah dan edit class : Player, Skill, Vehicle

---
+++ ProjectGameBalapan/Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+  // Attribute
+	[SerializeField] private string name;
+  [SerializeField] private Vehicle vehicle;
+  [SerializeField] private Skill[] skill;
+  // Property
+  public string Name
+  {
+    get
+    {
+      return name;
+    }
+    set
+    {
+      name = value;
+    }
+  }
+  public Vehicle Vehicle
+  {
+    get
+    {
+      return vehicle;
+    }
+    set
+    {
+      vehicle = value;
+    }
+  }
+  // Method
+  public void UseSkill(int index)
+  {
+
+  }
+}
