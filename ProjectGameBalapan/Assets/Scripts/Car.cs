a643f3300faf1599660d2c27e67c5c0a8ad25ac1
R-Lan Kurnia <33871129+krlan2789@users.noreply.github.com>
2020-01-11 10:59:03 +0700

    Update concept.txt

--- ProjectGameBalapan/Assets/Scripts/Car.cs
+++ ProjectGameBalapan/Assets/Scripts/Car.cs
@@ -6,28 +6,12 @@ public class Car : MonoBehaviour
 {
 	[SerializeField]	private float speed, healthPoint;
 	[SerializeField]	private string name;
-	public float Speed
-	{
-		get
-		{
-			return speed;
-		}
-		set
-		{
-			speed = value;
-		}
-	}
 
   public string Name
   {
     get
     {
       return name;
-    }
-
-    set
-    {
-      name = value;
     }
   }
 }
