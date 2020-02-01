97186748c11531d648e9bf0724421de786d4bc1f
R-Lan Kurnia <33871129+krlan2789@users.noreply.github.com>
2020-02-01 19:00:53 +0700

    Add Script Controller and Gameplay

--- ProjectGameBalapan/Assets/Scripts/Controller.cs
+++ ProjectGameBalapan/Assets/Scripts/Controller.cs
@@ -4,16 +4,48 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour 
 {
+	// Atribut
+	private Rect leftArea, rightArea, bottomArea;
+	private static Controller instance;
 
-	// Use this for initialization
-	public void Start () 
+	// Properti
+	public static Controller Instance
 	{
-		
+		set
+		{
+			if(instance == null)
+			{
+				instance = FindObjectOfType<Controller>();
+				instance.Initialize();
+			}
+		}
+	}
+
+	// Method
+	private void Initialize() 
+	{
+		// Area Layar sentuh kiri / Belok kiri
+		leftArea = new Rect(0, Screen.height * .15f, Screen.width / 2, Screen.height * .72f);
+		// Area Layar sentuh kanan / Belok kanan
+		rightArea = new Rect(Screen.width / 2, Screen.height * .15f, Screen.width / 2, Screen .height * .72f);
+		// Area Layar sentuh bawah / Rem
+		bottomArea = new Rect(0, 0, Screen.width, Screen.height * .15f);
 	}
 	
-	// Update is called once per frame
-	public void Update () 
+	public void ChangeDirection (Vector3 input)
 	{
-		
+		Debug.Log(input);
+		if (leftArea.Contains(input))
+		{
+			Debug.Log("Left : " + leftArea);
+		}
+		if (rightArea.Contains(input))
+		{
+			Debug.Log("Right : " + rightArea);
+		}
+		if (bottomArea.Contains(input))
+		{
+			Debug.Log("Bottom : " + bottomArea);
+		}
 	}
 }
