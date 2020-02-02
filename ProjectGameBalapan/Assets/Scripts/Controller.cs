4e4702e199913ca7199d2120580739085cb3e291
R-Lan Kurnia <33871129+krlan2789@users.noreply.github.com>
2020-02-02 22:47:57 +0700

    Update Controller

--- ProjectGameBalapan/Assets/Scripts/Controller.cs
+++ ProjectGameBalapan/Assets/Scripts/Controller.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour 
 {
-	// Atribut
+	#region Atribut
+	// Area kontrol layar sentuh
 	private Rect leftArea, rightArea, bottomArea;
+	// Objek yang dikendalikan
+	private Player player;
 	private static Controller instance;
+	#endregion
 
-	// Properti
+	#region Properti
 	public static Controller Instance
 	{
 		set
@@ -16,36 +20,45 @@ public class Controller : MonoBehaviour
 			if(instance == null)
 			{
 				instance = FindObjectOfType<Controller>();
-				instance.Initialize();
+				instance.Init();
 			}
 		}
 	}
+	#endregion
 
-	// Method
-	private void Initialize() 
+	#region Method
+	// Menginisialisasi
+	private void Init() 
 	{
+	}
+	// Mengubah Arah
+	private void Update ()
+	{
+		// Mencari Gameobject yang terdapat Class Player
+		player = FindObjectOfType<Player>();
 		// Area Layar sentuh kiri / Belok kiri
 		leftArea = new Rect(0, Screen.height * .15f, Screen.width / 2, Screen.height * .72f);
 		// Area Layar sentuh kanan / Belok kanan
 		rightArea = new Rect(Screen.width / 2, Screen.height * .15f, Screen.width / 2, Screen .height * .72f);
 		// Area Layar sentuh bawah / Rem
 		bottomArea = new Rect(0, 0, Screen.width, Screen.height * .15f);
+		
+		if(Input.GetMouseButtonDown(0))
+		{
+			Debug.Log(Input.mousePosition);
+			if (leftArea.Contains(Input.mousePosition))
+			{
+				Debug.Log("Left : " + leftArea);
+			}
+			if (rightArea.Contains(Input.mousePosition))
+			{
+				Debug.Log("Right : " + rightArea);
+			}
+			if (bottomArea.Contains(Input.mousePosition))
+			{
+				Debug.Log("Bottom : " + bottomArea);
+			}
+		}
 	}
-	
-	public void ChangeDirection (Vector3 input)
-	{
-		Debug.Log(input);
-		if (leftArea.Contains(input))
-		{
-			Debug.Log("Left : " + leftArea);
-		}
-		if (rightArea.Contains(input))
-		{
-			Debug.Log("Right : " + rightArea);
-		}
-		if (bottomArea.Contains(input))
-		{
-			Debug.Log("Bottom : " + bottomArea);
-		}
-	}
+	#endregion
 }
--- ProjectGameBalapan/Assets/Scripts/Vehicle.cs
+++ ProjectGameBalapan/Assets/Scripts/Vehicle.cs
@@ -2,7 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Vehicle
+enum VehicleType
 {
+  Ocean, Hell, Sky
+}
 
+public class Vehicle : MonoBehaviour
+{
+  #region Atribut
+  // Nama Kendaraan
+  [SerializeField] private string name;
+  // Kecepatan, Kekuatan, Pertahanan dari Kendaraan
+  [SerializeField] private int speed, power, defense;
+  // Poin Darah dari Kendaraan
+  [SerializeField] private float healthPoint;
+  // Tipe Kendaraan
+  [SerializeField] private VehicleType vehicleType;
+  // Objek Kendaraan
+  [SerializeField] private GameObject vehicle;
+  #endregion
+
+  #region Properti
+  public string Name
+  {
+    get
+    {
+      return name;
+    }
+  }
+  #endregion
+
+  #region Method
+  // Mengurangi Kecepatan hingga berhenti
+  public void Rem()
+  {
+  }
+  // Menjalankan Objek Kendaraan Secara Otomatis
+  public void Run()
+  {
+  }
+
+  public void UpdateDirection(Vector3 direction)
+  {
+  }
+
+  #endregion
 }
